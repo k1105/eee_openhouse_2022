abb5167c7994a8348e2fb5dc3d7a3b5e8848ac76
k1105 <kanapon1105@yahoo.co.jp>
2022-07-22 22:52:14 +0900

    余計なimport削除

--- src/sketches/DisplayFinger.js
+++ src/sketches/DisplayFinger.js
@@ -1,24 +1,10 @@
 import { ReactP5Wrapper } from "react-p5-wrapper";
-import { separatedFinger } from "./separatedFinger";
-import { connectedFinger } from "./connectedFinger";
-import { shrinkFinger } from "./shrinkFinger";
 import { spreadFinger } from "./spreadFinger";
-import { manyFinger } from "./manyFinger";
 import { organizeFinger } from "./organizeFinger";
-import { indicateCircle } from "./indicateCircle";
 import { pileFinger } from "./pileFinger";
 
 export const DisplayFinger = ({ predictionsRef }) => {
-  // const functions = [
-  //   separatedFinger,
-  //   connectedFinger,
-  //   shrinkFinger,
-  //   spreadFinger,
-  //   manyFinger,
-  //   organizeFinger,
-  // ];
   const functions = [spreadFinger, organizeFinger, pileFinger];
-  // const functions = [indicateCircle];
   let styleIndex = 0;
   let lostAt = 0;
   let lost = false;
@@ -47,19 +33,14 @@ export const DisplayFinger = ({ predictionsRef }) => {
   function sketch(p5) {
     p5.setup = () => {
       p5.createCanvas(window.innerWidth, window.innerHeight);
-      //p5.stroke(57, 127, 173);
-      //p5.stroke(245, 77, 1);
-      //p5.stroke(9, 178, 106);
+
       p5.stroke(220);
       p5.strokeWeight(10);
     };
 
     p5.draw = () => {
       let hands = [];
-      // p5.background(0)
       p5.background(57, 127, 173);
-      //p5.background(255);
-      // p5.background(215, 224, 235);
       p5.push();
       if (typeof predictionsRef.current == "object") {
         try {
@@ -87,15 +68,6 @@ export const DisplayFinger = ({ predictionsRef }) => {
           functions[styleIndex](p5, hands);
         } catch (e) {}
       }
-
-      // p5.push();
-      // // キャンバス左下にFPSを描画(小数点以下２桁を四捨五入)
-      // let fps = p5.frameRate();
-      // p5.fill(255);
-      // p5.noStroke();
-      // p5.text("FPS: " + fps.toFixed(2), 10, p5.height - 10);
-      // p5.pop();
-      // p5.pop();
     };
   }
   return <ReactP5Wrapper sketch={sketch} />;
